6c117e0d02ea22ea038e97e06f1ac43fe36c5348
Cassiano <dc01spring8@vm-java.season.local>
2017-05-30 22:38:39 -0300

    Controller

--- src/main/java/br/com/cassiano/springproject/config/SpringProjectConfig.java
+++ src/main/java/br/com/cassiano/springproject/config/SpringProjectConfig.java
@@ -1,8 +1,12 @@
 package br.com.cassiano.springproject.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.cassiano.springproject.service.HelloWorldService;
 import br.com.cassiano.springproject.service.HelloWorldServiceImpl;
@@ -10,7 +14,17 @@ import br.com.cassiano.springproject.user.HelloWorldUser;
 import br.com.cassiano.springproject.user.HelloWorldUserImpl;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan("br.com.cassiano.springproject")
 public class SpringProjectConfig {
+	
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
 
 	@Bean(name = "helloBean")
 	@Description("Description")
---
+++ src/main/java/br/com/cassiano/springproject/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.cassiano.springproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(){
+		return "home";
+	}
+	
+}
