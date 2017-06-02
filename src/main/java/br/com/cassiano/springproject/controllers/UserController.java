abb9e5c17cdae595ec11ad4ce137e3f7628c73a5
Cassiano <dc01spring8@vm-java.season.local>
2017-06-01 22:41:29 -0300

    jsp

--- src/main/java/br/com/cassiano/springproject/controllers/UserController.java
+++ src/main/java/br/com/cassiano/springproject/controllers/UserController.java
@@ -1,20 +1,30 @@
 package br.com.cassiano.springproject.controllers;
 
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.cassiano.springproject.entity.User;
 
 @Controller
 public class UserController {
 	
+	private List<User> users = new ArrayList<>();
+	
 	@RequestMapping("/users")
-	public String users(ModelMap map){
-		User user = new User();
-		
+	public ModelAndView users(ModelMap map){
+		ModelAndView model = new ModelAndView("user");
+		User user = new User();		
 		map.addAttribute("user",user);
-		return "user";
+		map.addAttribute("users",users);
+		return model;
 	}
 	
 	@RequestMapping("/user/save")
@@ -23,9 +33,11 @@ public class UserController {
 		System.out.println("First Name: " + user.getfirstName());
 		System.out.println("Last Name: " + user.getLastName());
 		System.out.println("CPF: " + user.getCpf());
+		users.add(user);
 		
 		user = new User();
 		map.addAttribute("user", user);
+		map.addAttribute("users",users);
 		return "user";
 	}
 
--- src/main/webapp/WEB-INF/views/pages/user.jsp
+++ src/main/webapp/WEB-INF/views/pages/user.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -12,7 +14,7 @@
 
 <h3> User Form</h3>
 
-<form:form modelAttribute="user" action="user/save" method="GET">
+<form:form modelAttribute="user" action="${pageContext.request.contextPath}/user/save" method="GET">
 
 	<div>
 		<label>Name:</label>
@@ -31,6 +33,23 @@
 	</div>
 
 </form:form>
+
+<h2>List of User</h2>
+
+<table>
+	<tr>
+		<td>Last Name</td>
+		<td>First Name</td>
+		<td>CPF</td>
+	</tr>
+	<c:forEach items="${users}" var="user">
+		<tr>
+			<td>${user.lastName}</td>
+			<td>${user.firstName}</td>
+			<td>${user.cpf}</td>
+		</tr>
+	</c:forEach>
+</table>
 
 </body>
 </html>
